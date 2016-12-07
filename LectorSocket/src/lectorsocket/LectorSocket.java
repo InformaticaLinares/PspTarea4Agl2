05333d00c061d86e99c68832577de7767beed31a
Ruben Dengan <rubenbarruelo@hotmail.com>
2016-12-07 14:04:45 +0100

    Cambio del Lector para que lea las 100 veces definitivamente

--- LectorSocket/src/lectorsocket/LectorSocket.java
+++ LectorSocket/src/lectorsocket/LectorSocket.java
@@ -23,7 +23,7 @@ public class LectorSocket {
         BufferedReader entrada = null; // Buffer para leer del stream
         String datosEntrada;  //para los valores que vamos leyendo del canal
         int id = Integer.parseInt(args[0]);
-        boolean conex = false;
+        boolean conex = false, leido = false;
         int num;
         String fichlog = "Resultado.txt";
         String ficherr = "Errores.txt";
@@ -44,35 +44,40 @@ public class LectorSocket {
         }
         ps.flush();
         pe.flush();
-        while (conex == false) {
-            try {
-                //Pido conexión al equipo a través del puerto 12500, donde escucha el escritor
-                canal = new Socket("localhost", 12500);
-                conex = true;
-            } catch (Exception e) {
-                System.err.println("Consumidor " + id + ". Error al conectar");
-                conex = false;
-                Thread.sleep(500);
+        while (leido == false) {
+            if (conex == false) {
+                try {
+                    //Pido conexión al equipo a través del puerto 12500, donde escucha el escritor
+                    canal = new Socket("localhost", 12500);
+                    conex = true;
+                } catch (Exception e) {
+                    System.err.println("Consumidor " + id + ". Error al conectar");
+                    conex = false;
+                    Thread.sleep(500);
+                }
+            } else {
+                try {
+                    //Hay conexión --> necesito stream de entrada en el canal
+                    //Lector con Buffer para no perder ningún dato
+                    entrada = new BufferedReader(new InputStreamReader(canal.getInputStream()));
+                    //Vamos a leer mientras tengamos datos que leer
+                    datosEntrada = entrada.readLine();
+
+                    num = Integer.parseInt(datosEntrada);
+                    System.out.println("Consumidor " + id + ". Nº Leido: " + num);
+                    leido=true;
+                    entrada.close();
+
+                } catch (IOException e) {
+                    Thread.sleep(100);
+                    System.out.println("\nConsumidor " + id + ". Error al leer del canal\n");
+                    conex=false;
+                    leido=false;
+                    //System.err.println(e.toString()); //descripcion de lo que pasa
+                }
+                canal.close();
             }
         }
-        try {
-            //Hay conexión --> necesito stream de entrada en el canal
-            //Lector con Buffer para no perder ningún dato
-            entrada = new BufferedReader(new InputStreamReader(canal.getInputStream()));
-            //Vamos a leer mientras tengamos datos que leer
-            datosEntrada = entrada.readLine();
-
-            num = Integer.parseInt(datosEntrada);
-            System.out.println("Consumidor " + id + ". Nº Leido: " + num);
-
-            entrada.close();
-
-        } catch (IOException e) {
-            System.out.println("Consumidor " + id + ". Error al leer del canal");
-
-            //System.err.println(e.toString()); //descripcion de lo que pasa
-        }
-        canal.close();
     }
 
 }
